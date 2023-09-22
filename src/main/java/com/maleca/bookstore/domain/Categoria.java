748e24943e6dd4f1138d39c2168c7eb5e65004b3
amaleca <alfredomaleca55@gmail.com>
2023-09-22 14:12:18 +0200

    finalizando as classes de dominio

--- src/main/java/com/maleca/bookstore/domain/Categoria.java
+++ src/main/java/com/maleca/bookstore/domain/Categoria.java
@@ -1,15 +1,30 @@
 package com.maleca.bookstore.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class Categoria {
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+
+@Entity(name="categoria")
+public class Categoria implements Serializable {
 	
+	private static  final long serialVersionId = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private String descricao;
 	
+	
+	@OneToMany(mappedBy = "categoria")
 	private List<Livro> livros = new ArrayList<>();
 
 	public Categoria() {
@@ -19,7 +34,7 @@ public class Categoria {
 
 	public Categoria(Integer id, String nome, String descricao) {
 		super();
-		this.id = id;
+		this.id = id; 
 		this.nome = nome;
 		this.descricao = descricao;
 	}
