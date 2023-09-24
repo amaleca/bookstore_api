97fbc7bb611309b3e44e2bc0d3fe5aebccb6bab4
amaleca <alfredomaleca55@gmail.com>
2023-09-24 12:05:10 +0200

    findAll para categoria utilizando padrao DTO

--- src/main/java/com/maleca/bookstore/service/CategoriaService.java
+++ src/main/java/com/maleca/bookstore/service/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.maleca.bookstore.service;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,6 +22,10 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException (
 				"Objecto nao encontrado! id: " + id + ", Tipo: "+ Categoria.class.getName()));
 		
+	}
+	
+	public List<Categoria> findAll(){
+		return repository.findAll();
 	}
 
 }
